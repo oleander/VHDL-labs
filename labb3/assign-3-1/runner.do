d5e0f9eaaf72a0e885bcf432b8e093274c24952b
Linus Oleander <oleander@remote1.student.chalmers.se>
2011-02-20 19:56:26 +0100

    Preparing for final release

--- labb3/assign-3-1/runner.do
+++ labb3/assign-3-1/runner.do
@@ -1,3 +1,2 @@
 restart -f
-force clk 1 0, 0 50 -repeat 100
 run 3000
--- labb3/assign-3-1/testb.vhd
+++ labb3/assign-3-1/testb.vhd
@@ -7,48 +7,59 @@ entity testb is
   port (testok : out std_logic := 'H');
 end entity;  -- testb
 architecture arch of testb is
-  signal clk, reset, en, d, q : std_logic;
+  signal reset, en, d, q : std_logic;
+  signal clk             : std_logic := '0';
 begin
+  
+  reset <= '1', '0' after 150 ns;
+  clk   <= not clk  after 100 ns;
   u1 : fdre port map(clk, reset, en, d, q);
 
   test : process
   begin
-    -- It should be possible to reset {q}
-    reset <= '1';
+    -- It should have a working {reset}
     wait for 200 ns;
     if q /= '0' then
       testok <= '0';
       report "error at time 200";
     end if;
-    
+
     -- It should be possible to set {q} when {en} is active
     en <= '1';
-    reset <= '0';
-    d <= '1';
+    d  <= '1';
     wait for 200 ns;
     if q /= '1' then
       testok <= '0';
       report "error at time 400";
     end if;
-    
-    -- It should ignore {d} if {en} is deactivated
+
+    -- It should ignore {d} if {en} is deactivated, q = 0
     en <= '0';
-    d <= '0';
+    d  <= '0';
     wait for 200 ns;
     if q /= '1' then
       testok <= '0';
       report "error at time 600";
     end if;
-    
+
     -- It should be possible to set {q} to {0} when {en} is active
     en <= '1';
-    d <= '0';
+    d  <= '0';
     wait for 200 ns;
     if q /= '0' then
       testok <= '0';
       report "error at time 800";
     end if;
     wait;
-    
+
+    -- It should ignore {d} if {en} is deactivated, q = 1
+    en <= '0';
+    d  <= '1';
+    wait for 200 ns;
+    if q /= '0' then
+      testok <= '0';
+      report "error at time 1000";
+    end if;
   end process;
+  
 end architecture;  -- arch

803bcd6fa01d0dbac3af4345e15163e9a4adf2e6
Linus Oleander <oleander@remote1.student.chalmers.se>
2011-02-20 19:32:37 +0100

    Adding some tests for D-latch

---
+++ labb3/assign-3-1/runner.do
@@ -0,0 +1,3 @@
+restart -f
+force clk 1 0, 0 50 -repeat 100
+run 3000
--- labb3/assign-3-1/testb.vhd
+++ labb3/assign-3-1/testb.vhd
@@ -4,7 +4,7 @@ use ieee.numeric_std.all;
 use work.mypackage.all;
 
 entity testb is
-  port (fail : out std_logic);
+  port (testok : out std_logic := 'H');
 end entity;  -- testb
 architecture arch of testb is
   signal clk, reset, en, d, q : std_logic;
@@ -13,16 +13,42 @@ begin
 
   test : process
   begin
-    clk   <= '1';
+    -- It should be possible to reset {q}
     reset <= '1';
     wait for 200 ns;
-
     if q /= '0' then
-      fail <= '0';
+      testok <= '0';
       report "error at time 200";
     end if;
-
-    wait;
-  end process ;
     
+    -- It should be possible to set {q} when {en} is active
+    en <= '1';
+    reset <= '0';
+    d <= '1';
+    wait for 200 ns;
+    if q /= '1' then
+      testok <= '0';
+      report "error at time 400";
+    end if;
+    
+    -- It should ignore {d} if {en} is deactivated
+    en <= '0';
+    d <= '0';
+    wait for 200 ns;
+    if q /= '1' then
+      testok <= '0';
+      report "error at time 600";
+    end if;
+    
+    -- It should be possible to set {q} to {0} when {en} is active
+    en <= '1';
+    d <= '0';
+    wait for 200 ns;
+    if q /= '0' then
+      testok <= '0';
+      report "error at time 800";
+    end if;
+    wait;
+    
+  end process;
 end architecture;  -- arch
